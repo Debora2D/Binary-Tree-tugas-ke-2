77791090bff25ec69461a07bafdb002d5721859e
Debora Rizky Mustikaningtyas <deborarizky14@gmail.com>
2017-11-22 05:54:02 +0700

    Create main.java

---
+++ src/binary/tree/tugas/ke2/main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binary.tree.tugas.ke2;
+
+import java.util.Scanner;
+public class main {
+    public static void main(String [] args){
+        Penguat x = new Penguat();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Masukkan jumlah data : ");
+        int y = in.nextInt();
+        for(int i = 0; i < y; i++){
+            System.out.println("Data ke " + (i+1) + " = ");
+            x.insert(in.nextInt());
+        }
+    }
+}
